c9e3c4e6902e375ca2d5c75dbf81ae49dd0d98f0
Katleho <kakhany@gmail.com>
2020-04-11 17:41:01 +0200

    Conditional view

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
@@ -17,6 +17,7 @@ function App() {
   }
 ]
 const [personState, setPersonState] = useState({persons});
+const [ showState, setVisibility] = useState(false);
 const swichNameHandler = (isName) => {
   persons[0].name = isName;
   setPersonState({
@@ -26,15 +27,34 @@ const swichNameHandler = (isName) => {
   console.log("Changed but not updating");
 }
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <button onClick={() => swichNameHandler("Katleho Archiebald")}>Press me</button>
-      <Person name={personState.persons[0].name} age={personState.persons[0].age}/>
-      <Person name={personState.persons[1].name} age={personState.persons[1].age} click={() => swichNameHandler("Testing")}>Testing hobbies</Person>
-      <Person name={personState.persons[2].name} age={personState.persons[2].age}/>
+const changeName = (event) => {
+  persons[1].name = event.target.value;
+  setPersonState({persons});
+}
+
+const togglePersonHandler = () => {
+  // const peopleDiv = document.querySelector(".allpeople");
+  // const nextView = (peopleDiv.style.display === "none") ? "block" : "none";
+  // peopleDiv.style.display = nextView;
+  setVisibility(!showState);
+}
+
+const personsDiv = showState ? (
+    <div className="allpeople">
+    <Person name={personState.persons[0].name} age={personState.persons[0].age} click={() => swichNameHandler()}/>
+    <Person name={personState.persons[1].name} age={personState.persons[1].age} change={changeName}>Testing hobbies</Person>
+    <Person name={personState.persons[2].name} age={personState.persons[2].age} click={() => swichNameHandler()}/>
     </div>
-  );
+  ) : null;
+
+return (
+  <div className="App">
+    <h1>Hi, I'm a React App</h1>
+    <button onClick={() => swichNameHandler("Katleho Archiebald")}>Press me</button>
+    <button onClick={togglePersonHandler}>Toggle person</button>
+    {personsDiv}
+  </div>
+);
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const Person = (props) => {
     return (
-        <div onClick={props.click}>
+        <div className="clickablediv">
             <h1>I'm {props.name} and I am {props.age} years old</h1>
             <p>{props.children}</p>
+            <input type="text" onChange={props.change} value={props.name}/>
         </div>
     )
 };
